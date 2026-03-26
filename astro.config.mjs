90569715513b7028201e1be772a89c6af740257a
Riley <rmcaldwell2015@gmail.com>
2026-03-26 10:43:34 -0400

    Redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://www.docksideweb.com",
-  adapter: cloudflare(),
   trailingSlash: 'always',
   output: "static",
   build: {
